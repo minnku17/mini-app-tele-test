bc4ecd2051044853400a1ea17f60f3a9699ffe59
Trương Hoà <truonghoa@MacBook-Pro-cua-Hoa.local>
2024-07-09 11:23:47 +0700

    add section connect wallet, countdown, description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,117 +1,73 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Countdown, { zeroPad } from "react-countdown";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex w-full min-h-screen flex-col items-center justify-center bg-white p-2 sm:p-0 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+      <div className="w-full sm:w-[574px] flex flex-col items-center justify-between border rounded-md border-[#e6e7eb] shadow-lg">
+        <div className="w-full flex flex-col px-[24px]">
+          <div className="w-full py-[26px]  flex items-center justify-between ">
+            <p className="text-[30px] font-semibold text-black">Giveaway</p>
+            <button className="py-[9px] px-[16px] bg-[#3396ff] border border-[#2e87e6] rounded-3xl text-[16px] text-white">
+              Connect Wallet
+            </button>
+          </div>
+          <div className="w-full px-[24px] flex items-center justify-center">
+            <Countdown
+              date={Date.now() + 24 * 60 * 60 * 1000 + 3 * 60 * 60 * 1000}
+              renderer={({ days, hours, minutes, seconds }) => {
+                return (
+                  <div className="flex items-center justify-center gap-4 text-black">
+                    <div className="w-16 h-16 flex flex-col items-center justify-center p-3 text-white rounded-xl bg-[#0184c7] dark:text-secondary text-secondary-foreground">
+                      <p className="text-[20px] font-bold">{zeroPad(days)}</p>
+                      <p className="text-[12px] font-light">Days</p>
+                    </div>
+                    <div className="w-16 h-16 flex flex-col items-center justify-center p-3 text-white rounded-xl bg-[#0184c7] dark:text-secondary text-secondary-foreground">
+                      <p className="text-[20px] font-bold">{zeroPad(hours)}</p>
+                      <p className="text-[12px] font-light">Hours</p>
+                    </div>
+                    <div className="w-16 h-16 flex flex-col items-center justify-center p-3 text-white rounded-xl bg-[#0184c7] dark:text-secondary text-secondary-foreground">
+                      <p className="text-[20px] font-bold">
+                        {zeroPad(minutes)}
+                      </p>
+                      <p className="text-[12px] font-light">Minutes</p>
+                    </div>
+                    <div className="w-16 h-16 flex flex-col items-center justify-center p-3 text-white rounded-xl bg-[#0184c7] dark:text-secondary text-secondary-foreground">
+                      <p className="text-[20px] font-bold">
+                        {zeroPad(seconds)}
+                      </p>
+                      <p className="text-[12px] font-light">Seconds</p>
+                    </div>
+                  </div>
+                );
+              }}
             />
-          </a>
+          </div>
+          <div className="w-full py-[16px] flex flex-col gap-3 items-center justify-between">
+            <p className="text-[18px] font-semibold text-black text-center">
+              Bitcoin $30,000 Airdrop
+            </p>
+            <p className="text-[14px] text-[#6b7280] text-center">
+              Bitcoin on BSC $30,000 Airdrop Is Now Live🚨
+            </p>
+            <p className="text-[14px] text-[#6b7280] text-center">
+              Follow the steps below to enter for the chance to win one of 10 x
+              $3,000 of $BTCBNB! 🎉⬇️
+            </p>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="border-t border-[#e6e7eb] rounded-b-md bg-[#f3f4f6] py-[16px] w-full flex items-center justify-center gap-2">
+          <p className="text-black">Powered by</p>
+          <img
+            className="h-[1.5rem] w-auto"
+            src="https://www.pinky.finance/images/logo.svg"
+          />
+        </div>
       </div>
     </main>
   );
